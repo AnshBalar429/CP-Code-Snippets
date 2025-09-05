8646eaeebc42a278bb93ecb1ddf13742a911072f
AnshBalar429 <anshbalar2910@gmail.com>
2025-09-05 11:02:00 +0530

    Update theme

--- Codeforces-Dark-Theme/script.js
+++ Codeforces-Dark-Theme/script.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Codeforces dark theme
-// @version      1.1.2
-// @description  dark theme for Codeforces
+// @version      1.1.1
+// @description  dark theme for codeforces
 // @author       Gaurang Tandon
 // @match        https://codeforces.com/*
 // @match        http://codeforces.com/*
 // @match        https://calendar.google.com/calendar/embed*
 // @match        https://www.facebook.com/v2.8/plugins/like.php*
-// @resource     desertCSS  https://raw.githubusercontent.com/GaurangTandon/codeforces-darktheme/master/desert.css
+// @resource     desertCSS  desert.css
 // @resource     monokaiEditorTheme https://raw.githubusercontent.com/ajaxorg/ace/62e8e9e4d3b21552dd83d454b7fd55d4981d096a/lib/ace/theme/monokai.css
-// @resource     darkthemecss https://raw.githubusercontent.com/GaurangTandon/codeforces-darktheme/master/darktheme.css
+// @resource     darkthemecss darktheme.css
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @run-at       document-start
@@ -31,11 +31,15 @@
 	}
 
 	if (window.self != window.top && /calendar\.google\.com/.test(window.self.location.hostname)) {
+		// cannot add the other styles as they interfere with
+		// calendar's elements (since the selectors are so generic)
 		GM_addStyle(`
 /* google calendar logo, see #13 */
 div.logo-plus-button {
     filter: invert(1) hue-rotate(180deg);
 }`);
+		// rest of the google calendar has already been inverted
+		// so return
 		return;
 	}
 
@@ -45,12 +49,17 @@ div.logo-plus-button {
 	GM_addStyle(style);
 	GM_addStyle(desertCSS);
 
+	// to avoid long FOUT duration
 	function applyFuncWhenElmLoaded(sel, func) {
 		var elm = document.querySelectorAll(sel);
 		if (!elm || elm.length == 0) return setTimeout(applyFuncWhenElmLoaded, 100, sel, func);
 		for (let i = 0, len = elm.length; i < len; i++) func(elm[i]);
 	}
 
+	// some properties are added via element.style
+	// need to override them via javascript
+
+	// div div h3 a = the top header "@user's blog" whose color property is added via js
 	applyFuncWhenElmLoaded(
 		"#pageContent div div h3 a, .comment-table.highlight-blue .right .ttypography p, .comment-table.highlight-blue .right .info",
 		function (elm) {
@@ -71,6 +80,7 @@ div.logo-plus-button {
 		elm.classList.add("dark");
 	});
 
+	// in this case !important doesn't workthrough css stylesheet
 	applyFuncWhenElmLoaded(".unread td", function (elm) {
 		elm.style.backgroundColor = "#13203a !important";
 	});
@@ -100,6 +110,9 @@ div.logo-plus-button {
 			elm.classList.remove(aceChromeClass);
 			elm.classList.add("ace-monokai");
 
+			// using a mutationobserver to revert addition of class ace-chome
+			// goes into an infinite loop, presumably because the script run
+			// by codeforces adds it back
 			function checkAceClassRemoved() {
 				if (elm.classList.contains(aceChromeClass)) {
 					elm.classList.remove(aceChromeClass);
@@ -131,18 +144,19 @@ div.logo-plus-button {
 
 	(function fixBlackTextInRightTableDuringContest() {
 		applyFuncWhenElmLoaded(".rtable span", function (elm) {
-			if (elm.style && elm.style.color == "rgb(0, 0, 0)") {
+			if (elm.style && elm.style.color == "rgb(0, 0, 0)")
 				overrideStyleAttribute(elm, "color", colors.whiteTextColor);
-            }
 		});
 	})();
 
+	// cannot override through css since specifity issue
 	(function improveLinkColorInGreenAlerts() {
 		applyFuncWhenElmLoaded("div.alert-success a", function (elm) {
 			overrideStyleAttribute(elm, "color", "#004794");
 		});
 	})();
 
+	// Throttle on both leading and trailing edge
 	function throttleFn(fn, delayMs = 100) {
 		let timeTrigger = 0, timeout = null;
 
@@ -163,22 +177,45 @@ div.logo-plus-button {
 		};
 	}
 
+	// The style is applied to multiple matching test-lines, hence cannot override
+	// via CSS because there is no regex-based selector.
 	(function improveTestCaseBgColor() {
+		/* Highlight both even and odd lines with the same color for consistency
+		with CF's behavior*/
+
+		// Behavior can be seen on
+		// 1. Single problem (https://codeforces.com/problemset/problem/1721/A)
+		// 2. Compact problemset (https://codeforces.com/contest/1721/problems)
 		const TEST_LINE_CLASS = "test-example-line";
 
+		/**
+		 * @param {HTMLElement} elm
+		 */
 		function applyBGToTestCaseLine(elm) {
-			const prop = 'cssText', value = 'background-color: rgb(26, 26, 26) !important;';
+      const prop = 'cssText', value = 'background-color: rgb(26, 26, 26) !important;';
+			// Color removed on hover-out by CF
 			if (elm.style[prop] === '') {
 				return;
 			}
+			// Ensure to not trigger MutObserver recursively
+			// Don't match the exact value in case different browsers
+			// stringify the cssText differently
 			if (!elm.style[prop].includes('(26') && !elm.style[prop].includes('#1a')) {
+				// Doesn't include both rgb(26,26,26) and #1a1a1a
 				elm.style[prop] = value;
 			}
 		}
 
-		function mutationCallback(mutationList) {
+		/**
+		 * Only called for `style` attribute modifications
+		 * when it spots a .test-example-line.
+		 * So not an expensive callback
+		 * @param {MutationRecord[]} mutationList
+		 * @param {MutationObserver} observer
+		 */
+		function mutationCallback(mutationList, observer) {
 			mutationList.forEach((mutation) => {
-				const target = (mutation.target);
+				const target = /** @type {HTMLElement} */ (mutation.target);
 				if (target.classList.contains(TEST_LINE_CLASS)) {
 					applyBGToTestCaseLine(target);
 				}
